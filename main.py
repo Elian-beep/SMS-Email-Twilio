7138d19d53796647511987fcd80151797cb382be
Elian-beep <elianoliver101@gmail.com>
2023-02-02 17:33:06 -0300

    prot send emails

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from twilio.rest import Client
 
-account_sid = 'ACa6e0fe1cd5f2ad6893420a24f8ada073'
-auth_token = '326cb82bf2732040b9b54138bcee71b0'
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 
+account_sid = ''
+auth_token = ''
+sendgrid_token = ''
+
+case_name = input("Your name: ")
+case_about = input("Your process: ")
+msg = f"Olá, meu nome é {case_name} e estou participando do {case_about}"
+
+# --------- (SMS) INRFORMAÇÕES DE CONTATO
 client = Client(account_sid, auth_token)
-
 from_number = '+13608003717'
-to_number = '+5592993169741'
-
 numbers = ['+5592985878449', '+5592993169741']
 # number_one = '+559791388065'
 # number_two = '+559288192972'
 # number_three = '+559293290162'
 
-case_name = input("Your name: ")
-case_about = input("Your process: ")
-msg = f"Olá, meu nome é {case_name} e estou participando do {case_about}"
+# --------- (EMAIL) INRFORMAÇÕES DO EMAIL
+from_email = Email("elian.19batista@gmail.com")
+emails = ["elianoliver101@gmail.com"]
+subject = "DESAFIO TALENT LAB ITACOATIARA"
+content = Content("text/plain", msg)
+sg = sendgrid.SendGridAPIClient(api_key=sendgrid_token)
 
 try:
     for i in range(len(numbers)):
@@ -23,19 +33,13 @@ try:
             from_=from_number,
             to=numbers[i],
             body=msg
-    )
+        )
     print(f'Mensagem enviada com sucesso! SID: {message.sid}')
+    # for i in range(len(emails)):
+    #     mail = Mail(from_email, To(emails[i]), subject, content)
+    #     response = sg.client.mail.send.post(request_body=mail.get())
+    #     # print(mail)
+    # # print(f'Email enviada com sucesso! SID: {message.sid}')
+
 except Exception as e:
     print(f'Não foi possível enviar a mensagem: {e}')
-
-
-# try:
-#     message = client.messages.create(
-#         from_=from_number,
-#         to=to_number,
-#         body=msg
-#     )
-
-#     print(f'Mensagem enviada com sucesso! SID: {message.sid}')
-# except Exception as e:
-#     print(f'Não foi possível enviar a mensagem: {e}')
